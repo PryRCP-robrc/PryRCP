2daf9a09d90c154ff41b60e2c639095ece0f6352
PryRCP-antcm <antonicano04@gmail.com>
2025-07-01 13:06:30 -0500

    Modificación de PeluqueroService con metodos CRUD

--- appointment-system/src/main/java/com/peluqueria/service/PeluqueroService.java
+++ appointment-system/src/main/java/com/peluqueria/service/PeluqueroService.java
@@ -1,30 +1,64 @@
 package com.peluqueria.service;
 
+
 import com.peluqueria.entity.Peluquero;
 import com.peluqueria.repository.PeluqueroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+
 import java.util.List;
 import java.util.Optional;
 
+
 @Service
-@Transactional(readOnly = true)
+@Transactional()
 public class PeluqueroService {
-    
+   
     @Autowired
     private PeluqueroRepository peluqueroRepository;
-    
+   
     public List<Peluquero> obtenerPeluquerosActivos() {
         return peluqueroRepository.findByActivoTrue();
     }
-    
+   
     public Optional<Peluquero> buscarPorId(Long id) {
         return peluqueroRepository.findById(id);
     }
-    
+   
     public List<Peluquero> obtenerTodos() {
         return peluqueroRepository.findAll();
+    }
+
+
+    // Crear un nuevo peluquero
+    public Peluquero crear (Peluquero peluquero){
+        peluquero.setId(null);
+        peluquero.setActivo(true);
+        return peluqueroRepository.save(peluquero);
+    }
+
+
+    //Actualizar un peluquero
+    public Optional<Peluquero> actualizar (Long id, Peluquero datosActualizados){
+        return peluqueroRepository.findById(id).map(peluquero -> {
+            peluquero.setNombre(datosActualizados.getNombre());
+            peluquero.setApellido(datosActualizados.getApellido());
+            peluquero.setEspecialidad(datosActualizados.getEspecialidad());
+            peluquero.setActivo(datosActualizados.getActivo());
+            return peluqueroRepository.save(peluquero);
+        });
+    }
+
+
+    //Eliminar un peluquero por ID
+    public boolean eliminar(Long id) {
+        if (peluqueroRepository.existsById(id)){
+            peluqueroRepository.deleteById(id);
+            return true;
+        }else{
+            return false;
+        }
     }
 }
