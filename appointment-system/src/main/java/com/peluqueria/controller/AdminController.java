03deaf58022a08e2d3f47a89b17e17f0fb7332d6
robinsonR18 <robinrc885@gmail.com>
2025-07-03 01:33:43 -0500

    crud servicio2
    arregle unos problemas al ejecutar el programa

--- appointment-system/src/main/java/com/peluqueria/controller/AdminController.java
+++ appointment-system/src/main/java/com/peluqueria/controller/AdminController.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.service.annotation.PatchExchange;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.math.BigDecimal;
